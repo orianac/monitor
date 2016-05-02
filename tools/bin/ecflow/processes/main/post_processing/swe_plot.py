b802f5687426aac634bb64a797636b08dad07444
Marisa Baptiste <mbapt@hydra.hydro.washington.edu>
2016-05-02 14:36:01 -0700

    second commit to git hub with cdf creator and plotting scripts

---
+++ tools/bin/ecflow/processes/main/post_processing/swe_plot.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python
+###### plotting swe percentiles
+###### usage: <python> <swe_plot.py> <configuration.cfg>
+
+###### Creates...
+
+from datetime import datetime, timedelta
+import numpy as np
+import pandas as pd
+import xarray as xr
+import os
+import time
+import math
+import gc
+import argparse
+from netCDF4 import Dataset
+import matplotlib
+matplotlib.use('Agg') 
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+from matplotlib.colors import ListedColormap
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+import cartopy.io.shapereader as shpreader
+from tonic.io import read_config
+
+
+
+#read in configuration file
+parser = argparse.ArgumentParser(description='Reorder dimensions')
+parser.add_argument('config_file', metavar='config_file', type=argparse.FileType('r'), nargs=1, help='configuration file')
+args = parser.parse_args()
+config_dict = read_config(args.config_file[0].name)
+
+#read in from configuration file
+direc = config_dict['VIC2NC']['OutputDirNC']
+N = config_dict['ECFLOW']['Met_Delay']
+cdf_loc = config_dict['PLOT']['cdf_SWE']
+plot_loc = config_dict['PLOT']['plot_SWE']
+
+N = int(N)
+#how many days behind metdata is from realtime
+
+date_unformat = datetime.now() - timedelta(days=N)
+
+date = date_unformat.strftime('%Y-%m-%d')
+date_ncfile = date_unformat.strftime('%Y%m%d')
+month_day = date_unformat.strftime("%B_%d")
+
+
+#read VIC output that has been converted to a netcdf file 
+file = "pnw_daily_run.%s-%s.nc" %(date_ncfile, date_ncfile)
+
+ds = xr.open_dataset(os.path.join(direc,file))
+
+
+#create list of all latitudes and longitudes for full recentangle
+un_lat = ds['lat'].values
+un_lon = ds['lon'].values
+
+num_lat = len(un_lat)
+num_lon = len(un_lon)
+
+latitude = np.repeat(un_lat,num_lon)
+longitude = np.tile(un_lon, num_lat)
+
+
+#Weibull Plotting Position
+q = []
+
+for i in range(1,151):
+    q.append(i/(150.0+1))
+   
+
+min_q = min(q)/2
+max_q = max(q)+min_q
+
+
+#select out time and variable data 
+ds_day = ds.sel(time=date)
+swe_ds = ds_day['SWE']
+
+
+#create a dictionary containing the lat, lon and corresponding percentile value (if one exists)
+d = []
+
+for i in range(0,len(latitude)):
+    
+    #iterate through all latitudes and longitudes
+    ds_lat = swe_ds.sel(lat=latitude[i])
+    ds_lon = ds_lat.sel(lon=longitude[i])
+    
+    value = ds_lon.values
+
+    lat = latitude[i]
+    lon = longitude[i]
+    #add to configuration file this location
+    try: 
+        cdf = pd.read_csv('%s/%s/%s_%s' %(cdf_loc, month_day, lat, lon), 
+                          index_col=None, delimiter=None, header=None)
+        
+        x = cdf[0]
+        
+        #10mm threshold
+        if (value < 10):
+            #combine = (lat, lon, percentile)
+            combine = (lat, lon)
+            d.append(combine)
+        else:    
+        
+            try:
+                #interpolation
+                f = interp1d(x,q)
+                f(value)
+                percentile = f(value)
+                combine = (lat, lon, float(percentile))
+                d.append(combine)
+            #if interpolation fails then a value is assigned based on whether it is higher or lower than the range
+            except ValueError:
+                if (value > max(x)):
+                    percentile = (max_q)
+                    combine = (lat, lon, percentile)
+                    d.append(combine)
+                else:
+                    percentile = (min_q)
+                    combine = (lat, lon, percentile)
+                    d.append(combine)
+    #if there is no cdf than that values is a NaN
+    except IOError:
+        percentile = value
+        combine = (lat, lon)
+        d.append(combine)
+
+df = pd.DataFrame(d, columns=["Latitude", "Longitude", "Percentile"])
+a = df['Percentile'].values
+new = a.reshape(195, 245)
+
+dsx = xr.Dataset({'percentile': (['lat', 'lon'], new)}, coords={'lon': (['lon'], un_lon), 'lat': (['lat'], un_lat)})
+
+
+plt.figure(figsize=(8,8))
+
+ax = plt.axes(projection=ccrs.Mercator(central_longitude=-120, min_latitude=40.7, max_latitude=49.3, globe=None)) 
+
+ax.set_extent([-109.5, -125.01, 40, 54.01],ccrs.Geodetic())
+
+gl=ax.gridlines(draw_labels=True, xlocs = [-100, -110, -115, -120, -125], ylocs = [40, 42, 45, 48, 50])
+gl.xlabels_top = False
+gl.ylabels_right = False
+
+gl.xformatter = LONGITUDE_FORMATTER
+gl.yformatter = LATITUDE_FORMATTER
+
+states_provinces = cfeature.NaturalEarthFeature(
+        category='cultural',
+        name='admin_1_states_provinces_lines',
+        scale='50m',
+        facecolor='none')
+
+country_borders = cfeature.NaturalEarthFeature(
+        category='cultural',
+        name='admin_0_boundary_lines_land',
+        scale='50m',
+        facecolor='none')
+
+land = cfeature.NaturalEarthFeature(
+    category='physical',
+    name='land',
+    scale='50m',
+    facecolor='gray')
+
+ocean = cfeature.NaturalEarthFeature(
+    category='physical',
+    name='ocean',
+    scale='50m',
+    facecolor='blue')
+
+ax.add_feature(states_provinces, edgecolor='black', zorder = 2)
+ax.add_feature(country_borders, edgecolor='black', zorder = 2)
+ax.add_feature(land,facecolor='gray', zorder = 1)
+ax.add_feature(ocean,facecolor='lightblue', zorder = 1)
+
+
+cmap = matplotlib.colors.ListedColormap(['darkred', 'red', 'tomato', 'lightsalmon', 'mistyrose', 
+                                         'yellow', 'lightsteelblue', 'cornflowerblue', 'royalblue', 'blue', 'navy'])
+cmap.set_bad('lightgrey')
+img=dsx['percentile'].plot(ax = ax, vmin = 0, vmax = 10, levels=[0, 0.01, 0.05, 0.1, 0.2, 0.35, 0.65, 0.8, 0.9, 0.95, 0.99, 1.0], add_colorbar=False, cmap=cmap, transform = ccrs.PlateCarree(), zorder=2)
+
+
+plt.suptitle('SWE Percentile (threshold = 10mm)\n (%s)'%(date), y=1., x=.5, fontsize=16, fontweight='bold')     
+
+cbar = plt.colorbar(img, cmap = cmap, shrink=.95, orientation='horizontal')
+labels = ['0','1', '5', '10', '20', '35', '65', '80', '90', '95', '99', '100']
+cbar.set_ticks([0, 0.01, 0.05, 0.1, 0.2, 0.35, 0.65, 0.8, 0.9, 0.95, 0.99, 1.0])
+cbar.set_ticklabels(labels)
+cbar.ax.tick_params(labelsize=12)
+cbar.ax.set_xlabel('percentile', fontsize=15)
+
+print("saving plot")
+plt.savefig('%s/SWE_%s' %(plot_loc, date_ncfile), format='png', bbox='tight')
+
+
+
---
+++ tools/cdf_creator/swe_cdf_creator.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+######swe_cdf_creator.py
+######usage: <python> <swe_cdf> <>
+
+##### Creates list of ascending list from long-term SWE VIC data.
+##### Period of time is defined in configuration file. 
+##### Applies a moving window-esque method to collect 
+##### 5 days of data at a time and then sorts that data. 
+##### February 29th data is removed before being sorted.
+##### February 28th (plus surround two days) is stored at February 29th. 
+
+import datetime as dt
+import numpy as np
+import pandas as pd
+import xray
+import os
+import time
+import math
+import gc
+import argparse
+
+from tonic.io import read_config
+
+
+#read in configuration file
+parser = argparse.ArgumentParser(description='Reorder dimensions')
+parser.add_argument('config_file', metavar='config_file', type=argparse.FileType('r'), nargs=1, help='configuration file')
+args = parser.parse_args()
+config_dict = read_config(args.config_file[0].name)
+
+#read in meterological data location
+direc = config_dict['CDF']['Historic_Direc']
+file = config_dict['CDF']['Historic_File']
+start_date = config_dict['CDF']['Start_Date']
+end_date = config_dict['CDF']['End_Date']
+latlon_list = config_dict['CDF']['LatLon_List']
+output_direc = config_dict['SWE']['Out_Dir']
+
+print(start_date)
+print(end_date)
+#load in netcdf file from which cdfs will be taken
+all_data = xray.open_dataset(os.path.join(direc,file))
+
+#slice to the desired time period
+data_year = all_data.sel(time=slice(start_date, end_date))
+
+#read in list of grid cells for the PNW
+coordinates = pd.read_csv(latlon_list, sep=' ', index_col=None, delimiter=None, header=None)
+
+latitude = coordinates[0]
+longitude = coordinates[1]
+
+#parse out start/end years, month, days
+s_year = start_date[0:5]
+s_month = start_date[6:8]
+s_day = start_date[9:11]
+s_date = '%s,%s,%s' %(s_year,s_month,s_day)
+
+print('%s,%s,%s' %(s_year,s_month,s_day))
+
+e_year = end_date[0:5]
+e_month = end_date[6:8]
+e_day = end_date[9:11]
+e_date = '%s,%s,%s' %(e_year,e_month,e_day)
+
+print('%s,%s,%s' %(e_year,e_month,e_day))
+
+#create plotting positions using Wiebull distribution
+
+q = []
+
+for i in range(1,151):
+    q.append(i/(150+1))
+
+#cdfs for SWE
+for i in range(0,len(latitude)):
+    lat_data = data_year.sel(lat=latitude[i])
+    lon_data = lat_data.sel(lon=longitude[i])
+    data_values = lon_data['SWE'].values
+   
+    #group by month and day
+    s = pd.Series(data_values, index=pd.date_range(start_date, end_date))
+    #remove February 29th data
+    ss = s[(s.index.month!=2) | (s.index.day!=29)]
+    gb = ss.groupby([ss.index.month, ss.index.day])
+
+
+    for g, data in gb:
+        #get current day's data along with surrounding 2 days
+        current_day = dt.datetime(year=2011, month=g[0], day=g[1])
+        yesterday = current_day - dt.timedelta(days=1)
+        tomorrow = current_day + dt.timedelta(days=1)
+        yesterday_2 = current_day - dt.timedelta(days=2)
+        tomorrow_2 = current_day + dt.timedelta(days=2)
+
+        cc = pd.concat([gb.get_group(g), gb.get_group((yesterday.month, yesterday.day)), gb.get_group((yesterday_2.month, yesterday_2.day)), gb.get_group((tomorrow.month, tomorrow.day)), gb.get_group((tomorrow_2.month, tomorrow_2.day))])
+        cc.sort()
+
+        month = current_day.strftime('%B')
+
+        lat = latitude[i]
+        lon = longitude[i]
+	print('%s %s %s' %(month, lat, lon))
+        filename = '%s_%s' %(lat, lon)
+
+        #save the 150 values by day and lat/lon
+        path2 = ('%s/%s_%s' %(output_direc, month, current_day.day))
+        if not os.path.exists(path2):
+            os.makedirs(path2)
+        savepath2 = os.path.join(path2, filename)
+        cc.to_csv(savepath2, sep=' ', header=False, index=None)
+
+
+#save February 28 data as February 29
+for i in range(0,len(latitude)):
+    lat_data = data_year.sel(lat=latitude[i])
+    lon_data = lat_data.sel(lon=longitude[i])
+    data_values = lon_data['SWE'].values
+
+    s = pd.Series(data_values, index=pd.date_range(start_date, end_date))
+    ss = s[(s.index.month!=2) | (s.index.day!=29)]
+    gb = ss.groupby([ss.index.month, ss.index.day])
+
+
+    g = (2,28)
+    #get current day's data along with surrounding 2 days
+    current_day = dt.datetime(year=2011, month=g[0], day=g[1])
+    yesterday = current_day - dt.timedelta(days=1)
+    tomorrow = current_day + dt.timedelta(days=1)
+    yesterday_2 = current_day - dt.timedelta(days=2)
+    tomorrow_2 = current_day + dt.timedelta(days=2)
+
+    cc = pd.concat([gb.get_group(g), gb.get_group((yesterday.month, yesterday.day)), gb.get_group((yesterday_2.month, yesterday_2.day)), gb.get_group((tomorrow.month, tomorrow.day)), gb.get_group((tomorrow_2.month, tomorrow_2.day))])
+    cc.sort()
+
+    month = current_day.strftime('%B')
+
+    lat = latitude[i]
+    lon = longitude[i]
+
+    filename = '%s_%s' %(lat, lon)
+
+    #save the 150 values by day and lat/lon
+    path2 = ('%s/%s_%s' %(output_direc, month, current_day.day))
+    if not os.path.exists(path2):
+        os.makedirs(path2)
+    savepath2 = os.path.join(path2, filename)
+    cc.to_csv(savepath2, sep=' ', header=False, index=None)
+
+
+
