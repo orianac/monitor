cd3a642da81737f402c72e3fd455b2c3bad403d9
Hörður Bragi Helgason <helgason@hydra.hydro.washington.edu>
2017-01-30 14:14:15 -0800

    Revert "Committing post processing scripts"
    This reverts commit 0d3a4aa22b4a49bbf00483c3b9f5be11fe8108a6.

--- tools/bin/ecflow/processes/main/post_processing/swe_analysis.py
+++ tools/bin/ecflow/processes/main/post_processing/swe_analysis.py
@@ -6,9 +6,9 @@ usage: <python> <swe_plot.py> <configuration.cfg>
 Reads in a netcdf file converted from VIC fluxes.
 Extracts one day's data based on date in config file.
 Reads in saved cdfs.
-Interpolates every value's percentile 
+Interpolates every value's  percentile 
 based on where it falls relative to historic range. 
-Save as new netcdf file.
+Save as new netcdf file to be read by plotting script.
 """
 from datetime import datetime, timedelta
 import numpy as np
@@ -22,34 +22,33 @@ import argparse
 from scipy.interpolate import interp1d
 from netCDF4 import Dataset
 from tonic.io import read_config
-from collections import OrderedDict
 
 # read in configuration file
-parser = argparse.ArgumentParser(description='Calculate SWE percentiles')
+parser = argparse.ArgumentParser(description='Reorder dimensions')
 parser.add_argument('config_file', metavar='config_file',
-                    help='configuration file')
+                    type=argparse.FileType('r'), nargs=1, help='configuration file')
 args = parser.parse_args()
-config_dict = read_config(args.config_file)
+config_dict = read_config(args.config_file[0].name)
 
 # read in from configuration file
-direc = config_dict['VIC']['OutputDirRoot']
-n_days = int(config_dict['ECFLOW']['Met_Delay'])
+direc = config_dict['VIC2NC']['OutputDirNC']
+N = int(config_dict['ECFLOW']['Met_Delay'])
 
 # number of plotting positions
-num_pp = int(config_dict['PERCENTILES']['num_plot_pos'])
-cdf_loc = config_dict['PERCENTILES']['cdf_SWE']
-outfile_loc = config_dict['PERCENTILES']['Percentile_Loc']
-
-vic_start_date = config_dict['VIC']['vic_start_date']
+num_pp = int(config_dict['PLOT']['num_plot_pos'])
+cdf_loc = config_dict['PLOT']['cdf_SWE']
+outfile = config_dict['PLOT']['Percent_SWE']
 
 # how many days behind metdata is from realtime
-date_unformat = datetime.now() - timedelta(days=n_days)
+date_unformat = datetime.now() - timedelta(days=N)
 
 date = date_unformat.strftime('%Y-%m-%d')
+date_ncfile = date_unformat.strftime('%Y%m%d')
 month_day = date_unformat.strftime("%B_%-d")
 
-# read VIC output
-f = "fluxes.%s.nc" % (vic_start_date)
+# read VIC output that has been converted to a netcdf file
+f = "pnw_daily_run.%s-%s.nc" % (date_ncfile, date_ncfile)
+
 ds = xr.open_dataset(os.path.join(direc, f))
 
 # create list of all latitudes and longitudes for full rectangle
@@ -74,7 +73,7 @@ max_q = max(q) + min_q
 
 # select out time and variable data
 ds_day = ds.sel(time=date)
-swe_ds = ds_day['OUT_SWE']
+swe_ds = ds_day['SWE']
 
 # create a dictionary containing the lat, lon and corresponding percentile
 # value (if one exists)
@@ -100,7 +99,7 @@ for lat, lon in zip(latitude, longitude):
 
         # 10mm threshold (this is based on current monitor)
 
-        if (value < 10) and (x.mean() < 10):
+        if (value < 10):
             combine = (lat, lon)
             d.append(combine)
         else:
@@ -130,19 +129,12 @@ for lat, lon in zip(latitude, longitude):
         d.append(combine)
 
 # Dictionary to DataFrame to Dataset
-df = pd.DataFrame(d, columns=["Latitude", "Longitude", "swepercentile"])
-a = df['swepercentile'].values
+df = pd.DataFrame(d, columns=["Latitude", "Longitude", "Percentile"])
+a = df['Percentile'].values
 new = a.reshape(195, 245)
 
-dsx = xr.Dataset({'swepercentile': (['lat', 'lon'], new)},
+dsx = xr.Dataset({'Percentile': (['lat', 'lon'], new)},
                  coords={'lon': (['lon'], un_lon), 'lat': (['lat'], un_lat)})
 
-dsx_attrs = OrderedDict()
-dsx_attrs['_FillValue'] = -9999
-dsx.swepercentile.attrs = dsx_attrs
-
-dsxx = dsx * 100
-
 # save to netcdf
-dsxx.to_netcdf(os.path.join(outfile_loc, 'vic-metdata_swepercentile_%s.nc' %
-                            (date)), mode='w', format='NETCDF4')
+dsx.to_netcdf(outfile, mode='w', format='NETCDF4')
