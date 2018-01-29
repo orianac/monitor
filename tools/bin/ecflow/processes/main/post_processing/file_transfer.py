dcb29a14cc29a7cae795c0f57035127068daf2c1
hhelgason <helgason@washington.edu>
2018-01-29 10:53:04 -0800

    minor changes to file_transfer.py and swe_cdf_creator.py

--- tools/bin/ecflow/processes/main/post_processing/file_transfer.py
+++ tools/bin/ecflow/processes/main/post_processing/file_transfer.py
@@ -11,6 +11,7 @@ import os
 import paramiko
 import argparse
 from tonic.io import read_config
+from datetime import datetime
 
 # read in configuration file
 parser = argparse.ArgumentParser(description='Download met data')
@@ -24,8 +25,12 @@ source_loc = config_dict['PERCENTILES']['Percentile_Loc']
 dest_loc = config_dict['PERCENTILES']['Percentile_Dest']
 date = config_dict['VIC']['vic_end_date']
 
+current_date = datetime.now()
+month = current_date.strftime("%B_%-d").upper()[:3]
+
 # set up a connection to the NKN network
 ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.load_host_keys(os.path.expanduser(
     os.path.join("~", ".ssh", "known_hosts")))
 ssh.connect('reacchdb.nkn.uidaho.edu', username='vicmet', password='cl!m@te')
@@ -33,15 +38,18 @@ sftp = ssh.open_sftp()
 
 # transfer the percentile files
 vars = ['swe', 'sm', 'tm']
-
 for var in vars:
-    sftp.put(os.path.join(source_loc, 'vic-metdata_%spercentile_%s.nc' %
-                          (var, date)), os.path.join(dest_loc, 'vic-metdata_%spercentile.nc' % (var)))
+    if date[-2:] == '01':
+        sftp.put(os.path.join(source_loc, 'vic-metdata_%spercentile_%s.nc' %
+                              (var, date)), os.path.join(dest_loc, 'vic-metdata_%spercentile_%s%s.nc' % (var, month, str(1))))
+    else:
+        sftp.put(os.path.join(source_loc, 'vic-metdata_%spercentile_%s.nc' %
+                              (var, date)), os.path.join(dest_loc, 'vic-metdata_%spercentilei_CURRENT.nc' % (var)))
 
 # create a .txt file to include the date
 f = sftp.open(os.path.join(dest_loc, 'lastDate.txt'), 'w+')
 f.write('%s/%s/%s' % (date[:4], date[5:7], date[8:10]))
-f.close
+f.close()
 
 sftp.close()
 ssh.close()
