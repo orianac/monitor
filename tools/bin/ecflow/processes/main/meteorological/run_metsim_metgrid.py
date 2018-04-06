44d4cca698dc4dedc7434355d8282d3a9e0bcff1
Elizabeth Clark <eclark2@uw.edu>
2018-04-06 11:17:10 -0700

    Added argument to run_metsim_metgrid.py for user to tell the program to look at a section (MONITOR, MED_FCST or SEAS_FCST) to determine which files to run through metsim. Deleted run_metsim_SeasFcst.py and run_metsim_MedFcst.py, which are now redundant with run_metsim_metgrid.py

--- tools/bin/ecflow/processes/main/meteorological/run_metsim_metgrid.py
+++ tools/bin/ecflow/processes/main/meteorological/run_metsim_metgrid.py
@@ -17,21 +17,25 @@ parser = argparse.ArgumentParser(description='Create configuration file ' +
                                  'for MetSim')
 parser.add_argument('config_file', metavar='config_file',
                     help='configuration file')
+parser.add_argument('time_horizon_type', help='MONITOR, MED_FCST, or ' +
+                    'SEAS_FCST. Should correspond to section header in ' +
+                    'config_file')
 args = parser.parse_args()
 config_dict = read_config(args.config_file)
+section = args.time_horizon_type
 
-state = config_dict['SUBDAILY']['Met_State_File']
-outstate = config_dict['SUBDAILY']['MedFcst_Met_State']
-forcing = config_dict['ECFLOW']['Orig_Met']
-old_config_file = config_dict['SUBDAILY']['MetSim_Template']
-new_config_file = config_dict['SUBDAILY']['MetSim_Cfg']
-start_date = config_dict['SUBDAILY']['Subd_Met_Start_Date']
-end_date = config_dict['SUBDAILY']['Subd_Met_End_Date']
-out_dir = config_dict['SUBDAILY']['Subd_Out_Dir']
+state = config_dict[section]['Met_State_File']
+outstate = config_dict[section]['Out_Met_State']
+forcing = config_dict[section]['Orig_Met']
+old_config_file = config_dict[section]['MetSim_Template']
+new_config_file = config_dict[section]['MetSim_Cfg']
+start_date = config_dict[section]['Start_Date']
+end_date = config_dict[section]['End_Date']
+out_dir = config_dict[section]['Subd_Out_Dir']
 
 kwargs = {'STARTDATE': start_date.replace('-', '/'),
           'ENDDATE': end_date.replace('-', '/'),
-          'FORCING': forcing, 'DOMAIN': config_dict['SUBDAILY']['GridFile'],
+          'FORCING': forcing, 'DOMAIN': config_dict['DOMAIN']['GridFile'],
           'INSTATE': state, 'OUTDIR': out_dir, 'OUTSTATE': outstate}
 model_tools.replace_var_pythonic_config(
     old_config_file, new_config_file, header=None, **kwargs)
