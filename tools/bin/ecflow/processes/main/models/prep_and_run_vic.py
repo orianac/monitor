1a9ab866a445908957a2255e7ec5b3d08f4f2923
Elizabeth Clark <eclark2@uw.edu>
2018-04-30 16:30:55 -0700

    Replace create_global_file.py with prep_and_run_vic.py and prep_and_run_vic_twice.py to update global file and also run VIC in the same script. prep_and_run_vic_twice.py divides the run into two parts: 1) start to vic_save_state date, and 2) vic_save_state date to end date. A state file is saved at the end of the former for use in the next day's monitor, and at the end of the latter for use in initializing a forecast.

---
+++ tools/bin/ecflow/processes/main/models/prep_and_run_vic.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+"""
+prep_and_run_vic.py
+usage: <python> <prep_and_run_vic.py> <configuration.cfg> <time_horizon_type>
+
+This script creates a global file from the template with the correct
+ model start and end dates.
+"""
+import argparse
+import subprocess
+import os
+from dateutil.parser import parse
+
+from tonic.io import read_config
+from monitor import model_tools
+
+
+def main():
+    ''' Make a VIC global file with correct model dates and run VIC '''
+    parser = argparse.ArgumentParser(description='Reorder dimensions')
+    parser.add_argument('config_file', metavar='config_file',
+                        help='configuration file')
+    parser.add_argument('time_horizon_type', help='MONITOR, MED_FCST, or ' +
+                        'SEAS_FCST. Should correspond to section header in ' +
+                        'config_file')
+    args = parser.parse_args()
+    config_dict = read_config(args.config_file)
+    section = args.time_horizon_type
+
+    # read in the desired variables from the config file
+    global_template = config_dict['DOMAIN']['GlobalFileTemplate']
+    global_file_path = config_dict[section]['GlobalFilePath']
+    # get important dates
+    vic_start_date = config_dict[section]['Start_Date']
+    vic_end_date = config_dict[section]['End_Date']
+    vic_save_state = config_dict[section]['vic_save_state']
+
+    forcing_prefix = os.path.join(
+        config_dict[section]['Subd_Out_Dir'],
+        'forcing_{0}-{1}.'.format(vic_start_date.replace('-', ''),
+                                  vic_end_date.replace('-', '')))
+
+    # parse out year, month, and day from the model dates, which have
+    # the form YYYY-MM-DD
+    start = parse(vic_start_date)
+    end = parse(vic_end_date)
+    save_state = parse(vic_save_state)
+
+    # generate the path to the initial state file
+    in_state = os.path.join(config_dict[section]['InStatePath'],
+                            'state.%s%s%s_00000.nc' % (
+                                vic_start_date[:4], vic_start_date[5:7],
+                                vic_start_date[8:10]))
+
+    kwargs = {
+        'Start_Year': start.year,
+        'Start_Month': start.month,
+        'Start_Day': start.day,
+        'End_Year': end.year,
+        'End_Month': end.month,
+        'End_Day': end.day,
+        'State_Year': save_state.year,
+        'State_Month': save_state.month,
+        'State_Day': save_state.day,
+        'In_State': in_state,
+        'State_Path': config_dict[section]['StatePath'],
+        'Result_Path': config_dict[section]['OutputDirRoot'],
+        'Forcing_Prefix': forcing_prefix}
+
+
+    model_tools.replace_var_pythonic_config(
+        global_template, global_file_path, header=None, **kwargs)
+
+    subprocess.run(['mpirun', '-np', config_dict['ECFLOW']['Ncores'],
+                    config_dict['ECFLOW']['Executable'], '-g',
+                    global_file_path])
+
+
+if __name__ == "__main__":
+    main()
