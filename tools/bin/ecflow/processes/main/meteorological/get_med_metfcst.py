98371e1212642e7abc83b743736ea775ed12d539
Elizabeth Clark <eclark2@uw.edu>
2018-05-04 15:14:26 -0700

    fixed medium range forecast url. broke forecast download scripts into more functions. added medium range forecast date updates in config file

--- tools/bin/ecflow/processes/main/meteorological/get_med_metfcst.py
+++ tools/bin/ecflow/processes/main/meteorological/get_med_metfcst.py
@@ -11,13 +11,15 @@ they are added back in. To start, we just download multi-model ensemble mean.
 """
 import os
 import argparse
-from datetime import datetime
+from datetime import datetime, timedelta
 import numpy as np
+import pandas as pd
 import xarray as xr
 from cdo import Cdo
 import cf_units
 
 from tonic.io import read_config
+from monitor import model_tools
 
 
 def recreate_attrs(met_ds):
@@ -86,84 +88,113 @@ def recreate_attrs(met_ds):
 
     # specific humidity
     met_ds.specific_humidity.attrs['units'] = "kg/kg"
-    met_ds.specific_humidity.attrs['description'] = "Daily Mean Specific Humidity"
+    met_ds.specific_humidity.attrs['description'] = "Daily Mean Specific " + \
+        "Humidity"
     met_ds.specific_humidity.attrs['height'] = "2 m"
 
     return met_ds
 
 
-# read in configuration file
-parser = argparse.ArgumentParser(description='Download met forecast data')
-parser.add_argument('config_file', metavar='config_file',
-                    help='configuration file')
-args = parser.parse_args()
-config_dict = read_config(args.config_file)
+def main():
+    ''' Download meteorological forecast data for 90-day forecast
+        from http://thredds.northwestknowledge.net:8080/thredds/catalog/
+        NWCSC_INTEGRATED_SCENARIOS_ALL_CLIMATE/cfsv2_metdata_90day/catalog.html
+    '''
+    # read in configuration file
+    parser = argparse.ArgumentParser(description='Download met forecast data')
+    parser.add_argument('config_file', metavar='config_file',
+                        help='configuration file')
+    args = parser.parse_args()
+    config_dict = read_config(args.config_file)
 
-# initialize cdo
-cdo = Cdo()
+    # initialize cdo
+    cdo = Cdo()
 
-# read in meteorological data location
-met_fcst_loc = config_dict['FORECAST']['MedFcst_Met_Loc']
-old_config_file = config_dict['ECFLOW']['old_Config']
-new_config_file = config_dict['ECFLOW']['new_Config']
+    # read in meteorological data location
+    met_fcst_loc = config_dict['MED_FCST']['Met_Loc']
+    old_config_file = config_dict['ECFLOW']['old_Config']
+    new_config_file = config_dict['ECFLOW']['new_Config']
 
-# read in grid_file from config file
-grid_file = config_dict['SUBDAILY']['GridFile']
+    # read in grid_file from config file
+    grid_file = config_dict['DOMAIN']['GridFile']
 
-# define variable names used when filling threads URL
-# an abbreviation and a full name is needed
-varnames = {'vs': 'wind_speed', 'tmmx': 'air_temperature',
-            'tmmn': 'air_temperature',
-            'srad': 'surface_downwelling_shortwave_flux_in_air',
-            'sph': 'specific_humidity', 'pr': 'precipitation_amount',
-            'pet': 'potential_evapotranspiration'}
-# define model names for file name
-modelnames = ['CFSv2']
-# ['NCAR', 'NASA', 'GFDL', 'GFDL-FLOR', 'ENSMEAN', 'CMC1', 'CMC2',
-#              'CFSv2']
-new_units = {'vs': 'm s-1', 'sph': 'kg kg-1',
-             'tmmx': 'degC', 'tmmn': 'degC', 'srad': 'W m-2',
-             'pr': 'mm', 'pet': 'mm'}
-old_units = {'vs': 'm s-1', 'sph': 'kg kg-1',
-             'tmmx': 'K', 'tmmn': 'K', 'srad': 'W m-2',
-             'pr': 'mm', 'pet': 'mm'}
+    # define variable names used when filling threads URL
+    # an abbreviation and a full name is needed
+    varnames = {'vs': 'wind_speed', 'tmmx': 'air_temperature',
+                'tmmn': 'air_temperature',
+                'srad': 'surface_downwelling_shortwave_flux_in_air',
+                'sph': 'specific_humidity', 'pr': 'precipitation_amount',
+                'pet': 'potential_evapotranspiration'}
+    # define model names for file name
+    modelnames = ['CFSv2']
+    # ['NCAR', 'NASA', 'GFDL', 'GFDL-FLOR', 'ENSMEAN', 'CMC1', 'CMC2',
+    #              'CFSv2']
+    new_units = {'vs': 'm s-1', 'sph': 'kg kg-1',
+                 'tmmx': 'degC', 'tmmn': 'degC', 'srad': 'W m-2',
+                 'pr': 'mm', 'pet': 'mm'}
+    old_units = {'vs': 'm s-1', 'sph': 'kg kg-1',
+                 'tmmx': 'K', 'tmmn': 'K', 'srad': 'W m-2',
+                 'pr': 'mm', 'pet': 'mm'}
 
-# download metdata from http://thredds.northwestknowledge.net
-for model in modelnames:
-    dlist = []
-    for var, name in varnames.items():
-        url = ('http://tds-proxy.nkn.uidaho.edu/thredds/dodsC/' +
-               'NWCSC_INTEGRATED_SCENARIOS_ALL_CLIMATE/bcsd-nmme/' +
-               'cfsv2_metdata_90day/' +
-               'cfsv2_metdata_forecast_%s_daily.nc' % (var))
-        print('Reading {0}'.format(url))
-        ds = xr.open_dataset(url)
-        if name == 'air_temperature':
-            # Change variable names so that tmmn and tmax are different
-            ds.rename({'air_temperature': var}, inplace=True)
-        dlist.append(ds)
-    merge_ds = xr.merge(dlist)
-    # MetSim requires time dimension be named "time"
-    merge_ds.rename({'day': 'time'}, inplace=True)
-    for var in ('tmmn', 'tmmx'):
-        units_in = cf_units.Unit(old_units[var])
-        units_out = cf_units.Unit(new_units[var])
-        # Perform units conversion
-        units_in.convert(merge_ds[var].values[:], units_out, inplace=True)
-        # Fix _FillValue after unit conversion
-        merge_ds[var].values[merge_ds[var].values < -30000] = -32767.
-    # MetSim requires time dimension be named "time"
-    merge_ds = merge_ds.transpose('time', 'lat', 'lon')
-    merge_ds = recreate_attrs(merge_ds)
-    # Make sure tmax >= tmin always
-    tmin = np.copy(merge_ds['tmmn'].values)
-    tmax = np.copy(merge_ds['tmmx'].values)
-    swap_values = ((tmin > tmax) & (tmax != -32767.))
-    merge_ds['tmmn'].values[swap_values] = tmax[swap_values]
-    merge_ds['tmmx'].values[swap_values] = tmin[swap_values]
+    # download metdata from http://thredds.northwestknowledge.net
+    for model in modelnames:
+        dlist = []
+        for var, name in varnames.items():
+            url = ('http://thredds.northwestknowledge.net:8080/thredds/dodsC/' +
+                   'NWCSC_INTEGRATED_SCENARIOS_ALL_CLIMATE/' +
+                   'cfsv2_metdata_90day/' +
+                   'cfsv2_metdata_forecast_%s_daily.nc' % (var))
+            print('Reading {0}'.format(url))
+            xds = xr.open_dataset(url)
+            if name == 'air_temperature':
+                # Change variable names so that tmmn and tmax are different
+                xds.rename({'air_temperature': var}, inplace=True)
+            dlist.append(xds)
+        merge_ds = xr.merge(dlist)
+        # MetSim requires time dimension be named "time"
+        merge_ds.rename({'day': 'time'}, inplace=True)
+        for var in ('tmmn', 'tmmx'):
+            units_in = cf_units.Unit(old_units[var])
+            units_out = cf_units.Unit(new_units[var])
+            # Perform units conversion
+            units_in.convert(merge_ds[var].values[:], units_out, inplace=True)
+            # Fix _FillValue after unit conversion
+            merge_ds[var].values[merge_ds[var].values < -30000] = -32767.
+        # MetSim requires time dimension be named "time"
+        merge_ds = merge_ds.transpose('time', 'lat', 'lon')
+        merge_ds = recreate_attrs(merge_ds)
+        # Make sure tmax >= tmin always
+        tmin = np.copy(merge_ds['tmmn'].values)
+        tmax = np.copy(merge_ds['tmmx'].values)
+        swap_values = ((tmin > tmax) & (tmax != -32767.))
+        merge_ds['tmmn'].values[swap_values] = tmax[swap_values]
+        merge_ds['tmmx'].values[swap_values] = tmin[swap_values]
 
-    #tmp_file = os.path.join(met_fcst_loc, 'tmp_file.nc')
-    #merge_ds.to_netcdf(tmp_file)
-    outfile = os.path.join(met_fcst_loc, '%s.nc' % (model))
-    print('Conservatively remap and write to {0}'.format(outfile))
-    cdo.remapcon(grid_file, input=merge_ds, output=outfile)
+        end_date = pd.to_datetime(merge_ds.time.values[-1])
+        start_date = datetime.strptime(config_dict['MONITOR']['End_Date'],
+                                       '%Y-%m-%d') + timedelta(days=1)
+        start_date_format = start_date.strftime('%Y-%m-%d')
+        end_date_format = end_date.strftime('%Y-%m-%d')
+        vic_save_state_format = (end_date + timedelta(days=1)).strftime('%Y-%m-%d')
+        # replace start date, end date and met location in the configuration
+        # file
+        kwargs = {'START_DATE': config_dict['MONITOR']['Start_Date'],
+                  'END_DATE': config_dict['MONITOR']['End_Date'],
+                  'VIC_SAVE_STATE': config_dict['MONITOR']['vic_save_state'],
+                  'MED_START_DATE': start_date_format,
+                  'MED_END_DATE': end_date_format,
+                  'MED_VIC_SAVE_STATE': vic_save_state_format,
+                  'SEAS_START_DATE': -9999,
+                  'SEAS_END_DATE': -9999,
+                  'SEAS_VIC_SAVE_STATE':-9999}
+        model_tools.replace_var_pythonic_config(old_config_file,
+                                                new_config_file,
+                                                header=None, **kwargs)
+
+        outfile = os.path.join(met_fcst_loc, '%s.nc' % (model))
+        print('Conservatively remap and write to {0}'.format(outfile))
+        cdo.remapcon(grid_file, input=merge_ds, output=outfile)
+
+
+if __name__ == "__main__":
+    main()
--- tools/bin/ecflow/processes/main/meteorological/get_seasonal_metfcst.py
+++ tools/bin/ecflow/processes/main/meteorological/get_seasonal_metfcst.py
@@ -21,76 +21,102 @@ import cf_units
 from tonic.io import read_config
 from monitor import model_tools
 
-# read in configuration file
 
-parser = argparse.ArgumentParser(description='Download met forecast data')
-parser.add_argument('config_file', metavar='config_file',
-                    help='configuration file')
-args = parser.parse_args()
-config_dict = read_config(args.config_file)
+def read_and_convert_data(model, var):
+    ''' Read data from URL for model and var, then convert variable units
+        to metric '''
+    url = ('http://tds-proxy.nkn.uidaho.edu/thredds/dodsC/' +
+           'NWCSC_INTEGRATED_SCENARIOS_ALL_CLIMATE/bcsd-nmme/' +
+           'dailyForecasts/' +
+           'bcsd_nmme_metdata_%s_forecast_%s_daily.nc' % (
+               model, var))
+    print('Reading {0}'.format(url))
+    xds = xr.open_dataset(url)
+    # Define units
+    if xds[var].attrs['units'] == 'F':
+        xds[var].attrs['units'] = 'degF'
+    new_units = {'was': 'm s-1', 'dps': 'degC', 'tasmean': 'degC',
+                 'tasmax': 'degC', 'tasmin': 'degC', 'rsds': 'W m-2',
+                 'pr': 'mm', 'pet': 'mm'}
+    units_in = cf_units.Unit(xds[var].attrs['units'])
+    units_out = cf_units.Unit(new_units[var])
+    # Perform units conversion
+    units_in.convert(xds[var].values[:], units_out, inplace=True)
+    xds[var].attrs['units'] = new_units[var]
+    return xds
 
-# initialize cdo
-cdo = Cdo()
 
-# read in meteorological data location
-met_fcst_loc = config_dict['SEAS_FCST']['Met_Loc']
-old_config_file = config_dict['ECFLOW']['old_Config']
-new_config_file = config_dict['ECFLOW']['new_Config']
-
-# read in grid_file from config file
-grid_file = config_dict['DOMAIN']['GridFile']
-
-# define variable names used when filling threads URL
-# an abbreviation and a full name is needed
-varnames = ['was', 'dps', 'tasmean', 'tasmax', 'tasmin', 'rsds',
-            'pr', 'pet']
-# define model names for file name
-modelnames = ['ENSMEAN']
-# ['NCAR', 'NASA', 'GFDL', 'GFDL-FLOR', 'ENSMEAN', 'CMC1', 'CMC2',
-#              'CFSv2']
-new_units = {'was': 'm s-1', 'dps': 'degC', 'tasmean': 'degC',
-             'tasmax': 'degC', 'tasmin': 'degC', 'rsds': 'W m-2',
-             'pr': 'mm', 'pet': 'mm'}
-
-# download metdata from http://thredds.northwestknowledge.net
-for model in modelnames:
-    dlist = []
-    for var in varnames:
-        url = ('http://tds-proxy.nkn.uidaho.edu/thredds/dodsC/' +
-               'NWCSC_INTEGRATED_SCENARIOS_ALL_CLIMATE/bcsd-nmme/' +
-               'dailyForecasts/' +
-               'bcsd_nmme_metdata_%s_forecast_%s_daily.nc' % (
-                   model, var))
-        print('Reading {0}'.format(url))
-        ds = xr.open_dataset(url)
-        if ds[var].attrs['units'] == 'F':
-            ds[var].attrs['units'] = 'degF'
-        units_in = cf_units.Unit(ds[var].attrs['units'])
-        units_out = cf_units.Unit(new_units[var])
-        # Perform units conversion
-        units_in.convert(ds[var].values[:], units_out, inplace=True)
-        ds[var].attrs['units'] = new_units[var]
-        dlist.append(ds)
-    end_date = pd.to_datetime(ds.time.values[-1])
-    merge_ds = xr.merge(dlist)
-    merge_ds = merge_ds.transpose('time', 'lat', 'lon')
-    start_date = datetime.strptime(config_dict['MED_FCST']['End_Date'], 
+def get_dates(config_dict, merge_ds):
+    ''' Define dates to use in updated configuration file. Start comes from
+        the last day of the medium range forecast, and end is taken from the
+        downloaded data. '''
+    end_date = pd.to_datetime(merge_ds.time.values[-1])
+    start_date = datetime.strptime(config_dict['MED_FCST']['End_Date'],
                                    '%Y-%m-%d') + timedelta(days=1)
     start_date_format = start_date.strftime('%Y-%m-%d')
     end_date_format = end_date.strftime('%Y-%m-%d')
-    vic_save_state_format = (end_date + timedelta(days=1)).strftime('%Y-%m-%d')
-    # replace start date, end date and met location in the configuration file
-    kwargs = {'START_DATE': config_dict['MONITOR']['Start_Date'],
-              'END_DATE': config_dict['MONITOR']['End_Date'],
-              'VIC_SAVE_STATE': config_dict['MONITOR']['vic_save_state'],
-              'MED_START_DATE': config_dict['MED_FCST']['Start_Date'],
-              'MED_END_DATE': config_dict['MED_FCST']['End_Date'], 
-              'MED_VIC_SAVE_STATE': config_dict['MED_FCST']['vic_save_state'],
-              'SEAS_START_DATE': start_date_format,
-              'SEAS_END_DATE': end_date_format,
-              'SEAS_VIC_SAVE_STATE': vic_save_state_format, 'FULL_YEAR': 'Year'}
-    model_tools.replace_var_pythonic_config(old_config_file, new_config_file,
-                                            header=None, **kwargs)
-    outfile = os.path.join(met_fcst_loc, '%s.nc' % (model))
-    print('Conservatively remap and write to {0}'.format(outfile))
-    cdo.remapcon(grid_file, input=merge_ds, output=outfile)
+    vic_save_state_format = (end_date + timedelta(days=1)).strftime(
+        '%Y-%m-%d')
+    # replace start date, end date and met location in the configuration
+    # file
+    return {'START_DATE': config_dict['MONITOR']['Start_Date'],
+            'END_DATE': config_dict['MONITOR']['End_Date'],
+            'VIC_SAVE_STATE': config_dict['MONITOR']['vic_save_state'],
+            'MED_START_DATE': config_dict['MED_FCST']['Start_Date'],
+            'MED_END_DATE': config_dict['MED_FCST']['End_Date'],
+            'MED_VIC_SAVE_STATE':
+            config_dict['MED_FCST']['vic_save_state'],
+            'SEAS_START_DATE': start_date_format,
+            'SEAS_END_DATE': end_date_format,
+            'SEAS_VIC_SAVE_STATE': vic_save_state_format}
+
+
+def main():
+    ''' Download seasonal meteorological forecasts from
+        http://proxy.nkn.uidaho.edu/thredds/catalog/
+        NWCSC_INTEGRATED_SCENARIOS_ALL_CLIMATE/bcsd-nmme/dailyForecasts/
+        catalog.html and reformat for use in MetSim '''
+
+    parser = argparse.ArgumentParser(description='Download met forecast data')
+    parser.add_argument('config_file', metavar='config_file',
+                        help='configuration file')
+    args = parser.parse_args()
+    config_dict = read_config(args.config_file)
+
+    # initialize cdo
+    cdo = Cdo()
+
+    # read in meteorological data location
+    met_fcst_loc = config_dict['SEAS_FCST']['Met_Loc']
+
+    # read in grid_file from config file
+    grid_file = config_dict['DOMAIN']['GridFile']
+
+    # define variable names used when filling threads URL
+    # an abbreviation and a full name is needed
+    varnames = ['was', 'dps', 'tasmean', 'tasmax', 'tasmin', 'rsds',
+                'pr', 'pet']
+    # define model names for file name
+    modelnames = ['ENSMEAN']
+    # ['NCAR', 'NASA', 'GFDL', 'GFDL-FLOR', 'ENSMEAN', 'CMC1', 'CMC2',
+    #              'CFSv2']
+    # download metdata from http://thredds.northwestknowledge.net
+    for model in modelnames:
+        dlist = []
+        for var in varnames:
+            dlist.append(read_and_convert_data(model, var))
+        merge_ds = xr.merge(dlist)
+        merge_ds = merge_ds.transpose('time', 'lat', 'lon')
+        kwargs = get_dates(config_dict, merge_ds)
+        # replace start date, end date and met location in the configuration
+        # file
+        model_tools.replace_var_pythonic_config(
+            config_dict['ECFLOW']['old_Config'],
+            config_dict['ECFLOW']['new_Config'], header=None, **kwargs)
+        outfile = os.path.join(met_fcst_loc, '%s.nc' % (model))
+        print('Conservatively remap and write to {0}'.format(outfile))
+        cdo.remapcon(grid_file, input=merge_ds, output=outfile)
+
+
+if __name__ == '__main__':
+    main()
