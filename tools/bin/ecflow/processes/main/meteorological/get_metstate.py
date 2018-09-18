25cadf62f3c54301b81fd6c31e9e7b99f80c5d9a
Elizabeth Clark <eclark2@uw.edu>
2018-09-18 16:11:33 -0700

    Fixing dates in the process to create the vic state.

--- tools/bin/ecflow/processes/main/meteorological/get_metstate.py
+++ tools/bin/ecflow/processes/main/meteorological/get_metstate.py
@@ -20,6 +20,32 @@ import cf_units
 
 from tonic.io import read_config
 
+def retrieve_data(var, year, num_lon, num_lat, num_day):
+    ''' create url for Northwest Knowlegdge Network gridMet & download '''
+    try:
+        url = ('http://thredds.northwestknowledge.net:8080' +
+               '/thredds/dodsC/MET/{0}/{0}_{1}.nc?lon[0:1:{2}],lat[0:1:{3}]'.format(
+                   var[0], year, num_lon, num_lat) +
+               ',day[{0}:1:{1}],'.format(num_day[0], num_day[1]) +
+               'crs[0:1:0],{0}[{1}:1:{2}][0:1:{3}][0:1:{4}]'.format(
+                   var[1], num_day[0], num_day[1], num_lat, num_lon))
+        # This should work moving forward when crs is a variable
+        xds = xr.open_dataset(url).drop('crs').transpose('day', 'lat', 'lon')
+    except:
+        try:
+            url = ('http://thredds.northwestknowledge.net:8080/thredds/dodsC' +
+                   '/MET/{0}/{0}_{1}.nc?lon[0:1:{2}],lat[0:1:{3}]'.format(
+                       var[0], year, num_lon, num_lat) +
+                   ',day[{0}:1:{1}],'.format(num_day[0], num_day[1]) +
+                   '{0}[{1}:1:{2}][0:1:{3}][0:1:{4}]'.format(
+                       var[1], num_day[0], num_day[1], num_lon, num_lat))
+            # This should work for variables that don't include crs yet
+            xds = xr.open_dataset(url).transpose('day', 'lat', 'lon')
+        except:
+            print('Could not download data from {0}'.format(url))
+            raise
+    return xds
+
 
 def main():
     ''' get 90 days of gridMet data leading up to the first day of VIC
@@ -77,26 +103,14 @@ def main():
         # download metdata from http://thredds.northwestknowledge.net
         met_dsets = dict()
         for var in varnames:
-            url_endyear = ("http://thredds.northwestknowledge.net:8080" +
-                           "/thredds/dodsC/MET/%s/%s_%s.nc?lon[0:1:%s]," % (
-                               var[0], var[0], state_end_year, num_lon) +
-                           "lat[0:1:%s],day[%s:1:%s]," % (
-                               num_lat, num_startofyear, state_end_day_num) +
-                           "%s[%s:1:%s]" % (
-                               var[1], num_startofyear, state_end_day_num) +
-                           "[0:1:%s][0:1:%s]" % (num_lon, num_lat))
+            ds_endyear = retrieve_data(var, state_end_year, num_lon, num_lat,
+                                     (num_startofyear, state_end_day_num))
 
-            ds_endyear = xr.open_dataset(url_endyear)
-            url_startyear = ("http://thredds.northwestknowledge.net:8080" +
-                             "/thredds/dodsC/MET/%s/%s_%s.nc?lon[0:1:%s]," % (
-                                 var[0], var[0], state_start_year, num_lon) +
-                             "lat[0:1:%s],day[%s:1:%s]," % (
-                                 num_lat, state_start_day_num, num_endofyear) +
-                             "%s[%s:1:%s]" % (
-                                 var[1], state_start_day_num, num_endofyear) +
-                             "[0:1:%s][0:1:%s]" % (num_lon, num_lat))
-            ds_startyear = xr.open_dataset(url_startyear)
+            ds_startyear = retrieve_data(var, state_start_year, num_lon, num_lat,
+                                       (state_start_day_num, num_endofyear))
             # concatenate the two datasets and add general attributes
+            # TODO - remove this once Katherine has updated the pre-2018 files to
+            # also have the crs variable
             xds = xr.concat([ds_startyear, ds_endyear], 'day')
             # place data in dict, the variable abbreviation is used as key
             met_dsets[var[0]] = xds
@@ -105,16 +119,10 @@ def main():
         # download metdata from http://thredds.northwestknowledge.net
         met_dsets = dict()
         for var in varnames:
-            url = ("http://thredds.northwestknowledge.net:8080" +
-                   "/thredds/dodsC/MET/%s/%s_%s.nc?lon[0:1:%s]," % (
-                       var[0], var[0], state_start_year, num_lon) +
-                   "lat[0:1:%s],day[%s:1:%s]," % (
-                       num_lat, state_start_day_num, state_end_day_num) +
-                   "%s[%s:1:%s]" % (
-                       var[1], state_start_day_num, state_end_day_num) +
-                   "[0:1:%s][0:1:%s]" % (num_lon, num_lat))
+            xds = retrieve_data(var, state_start_year, num_lon, num_lat,
+                             (state_start_day_num, state_end_day_num))
             # download data and add general attributes
-            xds = xr.open_dataset(url)
+            #xds = xr.open_dataset(url)
             # place data in dict, the variable abbreviation is used as key
             met_dsets[var[0]] = xds
     for var in ('tmmn', 'tmmx'):
@@ -131,7 +139,6 @@ def main():
         met_dsets[var].rename({'air_temperature': var}, inplace=True)
 
     merge_ds = xr.merge(list(met_dsets.values()))
-    merge_ds.transpose('day', 'lat', 'lon')
     # MetSim requires time dimension be named "time"
     merge_ds.rename({'day': 'time', 'tmmn': 't_min', 'tmmx': 't_max',
                      'precipitation_amount': 'prec'}, inplace=True)
