e04c612d585b4971f80a7bf6848376fd39d0e50c
Elizabeth Clark <eclark2@uw.edu>
2018-09-21 19:14:13 -0700

    Added functionality to do the seasonal forecasts as well. This involved figuring out how to calculate percentiles on the future monthly averages.

--- tools/bin/ecflow/processes/main/post_processing/storage_analysis_forecasts.py
+++ tools/bin/ecflow/processes/main/post_processing/storage_analysis_forecasts.py
@@ -14,7 +14,7 @@ from scipy import stats
 import xarray as xr
 import pandas as pd
 from tonic.io import read_config
-
+import calendar
 
 def run_percentileofscore(historical, current, var):
     ''' Remove bad data and run stats.percentileofscore(). For SWE,
@@ -75,7 +75,6 @@ def main():
     args = parser.parse_args()
     config_dict = read_config(args.config_file)
     section = args.time_horizon_type
-    # assign the analysis date differently depending on when you're calculating for the percentile
     if section == 'MED_FCST':
         model = 'CFSv2'
         med_met_fcst_loc = config_dict['MED_FCST']['Met_Loc']
@@ -83,33 +82,18 @@ def main():
     # Overwrite start_date to be one day after the final date of the monitor.
     # Overwrite end_date to be the final date of the medium range forecast.
         start_date = (datetime.strptime(config_dict['MONITOR']['End_Date'],
-                  '%Y-%m-%d') - timedelta(days=1))
+                  '%Y-%m-%d') + timedelta(days=1))
         start_date_format = start_date.strftime('%Y-%m-%d')
         infile = os.path.join(
             config_dict[section]['OutputDirRoot'], 'fluxes.{}.nc'.format(
                 start_date_format))
-#        print(analysis_date)
-    elif section == 'SEAS_FCST':
-        model = 'CFSv2'
-        med_met_fcst_loc = config_dict['MED_FCST']['Met_Loc']
-        med_fcst_ds = xr.open_dataset(os.path.join(med_met_fcst_loc, '%s.nc' % (model)))
-    # Overwrite start_date to be one day after the final date of the medium range forecast.
-        # Overwrite end_date to be the final date of the seasonal range forecast.
-        start_date = pd.to_datetime(med_fcst_ds['time'].values[-1]) +\
-                  timedelta(days=1)
-        start_date_format = start_date.strftime('%Y-%m-%d')
-        infile = os.path.join(
-            config_dict[section]['OutputDirRoot'], 'fluxes.{}.nc'.format(
-                start_date_format))
-#        analysis_date = start_date + timedelta(days=110)
-    print('open {}'.format(infile))
-    forecast_xds = xr.open_dataset(infile)
-    # TODO: determine Oct1 flux file name in script instead of as input
-    oct1_ds = xr.open_dataset(config_dict['MONITOR']['Oct1_flux_file'])
-    print('open {}'.format(config_dict['PERCENTILES']['historic_VIC_out']))
-    hist_xds = xr.open_dataset(config_dict['PERCENTILES']['historic_VIC_out'],
+        print('open {}'.format(infile))
+        forecast_xds = xr.open_dataset(infile)
+        # TODO: determine Oct1 flux file name in script instead of as input
+        oct1_ds = xr.open_dataset(config_dict['MONITOR']['Oct1_flux_file'])
+        print('open {}'.format(config_dict['PERCENTILES']['historic_VIC_out']))
+        hist_xds = xr.open_dataset(config_dict['PERCENTILES']['historic_VIC_out'],
                                chunks={'lat': 100, 'lon': 100})
-    if section == 'MED_FCST':
         for agg in [7, 15, 30]:
             curr_vals = {}
           #  for var in ['swe', 'sm', 'tm']:
@@ -124,33 +108,30 @@ def main():
                 oct_yr = start_date.year
             else:
                 oct_yr = start_date.year - 1
-                # extract variables
-            swe_subtracted = (forecast_xds['OUT_SWE'].rolling(time=agg,
-                        center=True).mean().isel(time=agg) -
-                        oct1_ds['OUT_SWE'].loc[dict(time='{}-10-01'.format(
-                            oct_yr))].values).values
-            template_array = (forecast_xds['OUT_SWE'].rolling(time=agg,
-                        center=True).mean().isel(time=agg) -
-                        oct1_ds['OUT_SWE'].loc[dict(time='{}-10-01'.format(
-                            oct_yr))].values)
             # fix the SWE values so that after the subtraction out of last year's Oct 1 snowpack
             # there is no negative value
+            swe_subtracted = (forecast_xds['OUT_SWE'] -
+                        oct1_ds['OUT_SWE'].loc[dict(time='{}-10-01'.format(
+                            oct_yr))].values).values
             swe_subtracted[ swe_subtracted < 0 ] = 0
-            print(np.nanmean(hist_xds['swe'].values))
-            curr_vals['swe'] = xr.DataArray(swe_subtracted,
-                                            coords=forecast_xds['OUT_SWE'].rolling(
-                                                          time=agg).mean().isel(time=agg).coords,
-                                            dims=['lat', 'lon'])
+            curr_vals['swe'] = (xr.DataArray(swe_subtracted,
+                                            coords=forecast_xds['OUT_SWE'].coords,
+                                            dims=['time', 'lat', 'lon'])).isel(time=slice(0,agg)).mean(dim='time')
+            curr_vals['swe'].to_netcdf('/civil/hydro/climate_toolbox/post_processing/cdf_results/us/junk.nc')
             curr_vals['sm'] = forecast_xds['OUT_SOIL_MOIST'].sum(dim='nlayer',
-                                         skipna=False).rolling(
-                                        time=agg, center=True).mean().isel(time=agg)
+                                         skipna=False).isel(time=slice(0,agg)).mean(dim='time')
             curr_vals['tm'] = curr_vals['swe'] + curr_vals['sm']
+            # this gets the analysis date to be in the middle of the period of interest
+            analysis_date = start_date + timedelta(days=int(agg/2))
             day = int(forecast_xds['OUT_SWE'].loc[dict(time=start_date)].time.dt.dayofyear)
-            print(day)
-            # Get historical CDF by selecting the five days centerd on the analysis day of year
+            # Get historical CDF by selecting the window centered on the analysis day of year
+            # with window width of the aggregation period (7, 15 or 30 days)
             hist_xds = hist_xds.where(
-                (hist_xds['time'].dt.dayofyear >= day - 2) &
-                (hist_xds['time'].dt.dayofyear <= day + 2))
+                (hist_xds['time'].dt.dayofyear >= day - int(agg/2)) &
+                (hist_xds['time'].dt.dayofyear <= day + int(agg/2)))
+            print(hist_xds.dims)
+            print(hist_xds.coords)
+            print('ABOVE!!!!!!!!')
             for var in ['swe', 'sm', 'tm']:
                 pname = '{0}day_{1}percentile'.format(agg, var)
                 percentiles = xr.Dataset({pname:
@@ -188,6 +169,116 @@ def main():
                 outfile = os.path.join(
                     config_dict[section]['Percentile_Loc'],
                     'vic-forecast_{0}_{1}.nc'.format(pname, start_date_format))
+                percentiles.to_netcdf(outfile)
+                print('{} saved'.format(outfile))
+
+
+    if section == 'SEAS_FCST':
+        # load in the medium range forecast file as well
+        model = 'CFSv2'
+        med_met_fcst_loc = config_dict['MED_FCST']['Met_Loc']
+        print(os.path.join(med_met_fcst_loc, '%s.nc' % (model)))
+        med_fcst_ds = xr.open_dataset(os.path.join(med_met_fcst_loc, '%s.nc' % (model)))
+    # Overwrite start_date to be one day after the final date of the monitor.
+    # Overwrite end_date to be the final date of the medium range forecast.
+        med_start_date = (datetime.strptime(config_dict['MONITOR']['End_Date'],
+                  '%Y-%m-%d') + timedelta(days=1))
+        med_start_date_format = med_start_date.strftime('%Y-%m-%d')
+        infile = os.path.join(
+            config_dict['MED_FCST']['OutputDirRoot'], 'fluxes.{}.nc'.format(
+                med_start_date_format))
+        # load medium fcst
+        med_forecast_xds = xr.open_dataset(infile)
+        # load seasonal fcst
+        model = 'CFSv2'
+    # Overwrite start_date to be one day after the final date of the medium range forecast.
+        # Overwrite end_date to be the final date of the seasonal range forecast.
+        seas_start_date = pd.to_datetime(med_fcst_ds['time'].values[-1]) +\
+                  timedelta(days=1)
+        seas_start_date_format = seas_start_date.strftime('%Y-%m-%d')
+        infile = os.path.join(
+            config_dict[section]['OutputDirRoot'], 'fluxes.{}.nc'.format(
+                seas_start_date_format))
+        print('open {}'.format(infile))
+        seas_forecast_xds = xr.open_dataset(infile)
+        merged_forecast_xds = xr.concat([med_forecast_xds, seas_forecast_xds], dim='time')
+        # TODO: determine Oct1 flux file name in script instead of as input
+        oct1_ds = xr.open_dataset(config_dict['MONITOR']['Oct1_flux_file'])
+        month_nums = merged_forecast_xds['OUT_SWE'].groupby('time.month').mean().month.values
+        # remove the start month since we only want month averages for the longer ranger forecast
+        month_nums = month_nums[month_nums!=med_start_date.month]
+        month_names = [calendar.month_abbr[month_integer] for month_integer in month_nums]
+        # get the months you'll need averaged over - they are the months after the current month
+        # use the combined med range and seasonal forecast files - merge the files with xarray?
+        # loop over those months
+        for (month_num, month_name) in zip(month_nums, month_names):
+            curr_vals = {}
+            print('get the variables we actually want')
+            title = {'swe': 'SWE', 'sm': 'Total column soil moisture',
+                     'tm': 'Total moisture storage'}
+                # remove Oct. 1 SWE at start of water year to correct for perennial SWE
+            if med_start_date.month >= 10:
+                oct_yr = med_start_date.year
+            else:
+                oct_yr = med_start_date.year - 1
+            # fix the SWE values so that after the subtraction out of last year's Oct 1 snowpack
+            # there is no negative value
+            swe_subtracted = (merged_forecast_xds['OUT_SWE'] -
+                        oct1_ds['OUT_SWE'].loc[dict(time='{}-10-01'.format(
+                            oct_yr))].values).values
+            swe_subtracted[ swe_subtracted < 0 ] = 0
+            curr_vals['swe'] = (xr.DataArray(swe_subtracted,
+                                            coords=merged_forecast_xds['OUT_SWE'].coords,
+                                            dims=['time', 'lat', 'lon'])).groupby('time.month').mean(
+                                             dim='time').sel(month=month_num)
+            curr_vals['sm'] = merged_forecast_xds['OUT_SOIL_MOIST'].sum(dim='nlayer',
+                                         skipna=False).groupby('time.month').mean(
+                                             dim='time').sel(month=month_num)
+            curr_vals['tm'] = curr_vals['swe'] + curr_vals['sm']
+            # this gets the analysis date to be in the middle of the period of interest
+            # Get historical CDF by selecting the window centered on the analysis day of year
+            # with window width of the aggregation period (7, 15 or 30 days)
+            print('open {}'.format(config_dict['PERCENTILES']['historic_VIC_out']))
+            hist_xds = xr.open_dataset(config_dict['PERCENTILES']['historic_VIC_out'],
+                               chunks={'lat': 100, 'lon': 100})
+            hist_xds = hist_xds.where(hist_xds['time'].dt.month == month_num)
+            for var in ['swe', 'sm', 'tm']:
+                pname = '{0}_average_{1}percentile'.format(month_name, var)
+                percentiles = xr.Dataset({pname:
+                                  (['lat', 'lon'],
+                                   np.nan * np.ones(curr_vals[var].shape)),
+                                  'category':
+                                  (['lat', 'lon'],
+                                   np.nan * np.ones(curr_vals[var].shape))},
+                                 coords={'lon': curr_vals[var].lon,
+                                         'lat': curr_vals[var].lat})
+                print('calculate percentiles for {}'.format(var))
+                percentiles[pname] = xr.apply_ufunc(
+                    run_percentileofscore, hist_xds[var], curr_vals[var],
+                    kwargs={'var': var}, input_core_dims=[['time'], []],
+                    dask='parallelized', output_dtypes=[float], vectorize=True)
+                print('calculate category for {}'.format(var))
+                percentiles['category'] = xr.apply_ufunc(return_category,
+                                                 percentiles[pname],
+                                                 dask='parallelized',
+                                                 output_dtypes=[float],
+                                                 vectorize=True)
+                print('add attributes')
+                percentiles[pname].attrs['_FillValue'] = np.nan
+                percentiles['category'].attrs['_FillValue'] = np.nan
+                percentiles.attrs['analysis_date'] = med_start_date_format
+                percentiles.attrs['title'] = '{} percentiles'.format(title[var])
+                percentiles.attrs['comment'] = ('Calculated from output from the'
+                                        ' Variable Infiltration '
+                                        'Capacity (VIC) Macroscale Hydrologic'
+                                        ' Model')
+                for attr in ['VIC_Model_Version', 'VIC_Driver',
+                     'references', 'Conventions', 'VIC_GIT_VERSION']:
+                    percentiles.attrs[attr] = merged_forecast_xds.attrs[attr]
+                print('save file')
+                outfile = os.path.join(
+                    config_dict[section]['Percentile_Loc'],
+                    'vic-forecast_{0}_{1}.nc'.format(pname, med_start_date_format))
                 percentiles.to_netcdf(outfile)
                 print('{} saved'.format(outfile))
 
